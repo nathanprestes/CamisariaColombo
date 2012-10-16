e74fead4fa560702c2c0a30517d57822b5172164
Nathan Prestes Costa <nathanprestes@homtail.com>
2012-10-16 19:11:52 -0300

    19:11 - 16/10/12

---
+++ js/carrinho.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('#ctl00_TopBar_TopBar1_pnlTopBarAssincrono .welcome').appendTo('#saudacao');
+	$('#ctl00_TopBar_TopBar1_pnlTopBarAssincrono .loginmsg').appendTo('#saudacao');
+	$('#menu01.menu').appendTo('#menu .menu-departamento');
+	$('.searchWrapper .search').first().appendTo('#search.search');
+});
